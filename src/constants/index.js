c7d98a20ecb470c13eeb1a1f9c0a86f65f856c66
Soham Sarkar <srutidhara153@gmail.com>
2025-07-01 21:55:42 +0530

    Portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,271 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Real Time Chat Application",
+    description:
+      "Talkify – A Real-Time Chat Application",
+    subDescription: [
+      "Real-time Messaging: Instant one-on-one chat powered by Socket.IO with typing indicators.",
+      "Secure Authentication: User sessions are protected using JWT and MongoDB Atlas.",
+      "Media Sharing: Supports image uploads up to 10MB using Cloudinary for fast delivery.",
+      "Customizable UI: Built with Tailwind CSS and DaisyUI, offering 32+ dynamic themes.",
+    ],
+    href: "https://talkify-pvdl.onrender.com/",
+    logo: "",
+    image: "/assets/projects/ChatApp.png",
+    tags: [
+      {
+        id: 1,
+        name: "Nodejs",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongoDB.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 5,
+        name: "Socket.io",
+        path: "/assets/logos/socket.io.svg",
+      },
+      {
+        id: 6,
+        name: "Express",
+        path: "/assets/logos/express.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "We Chat - Social Media UI Design",
+    description:
+      "A clean and responsive social media UI prototype built with HTML, CSS, and JavaScript to showcase my front-end design skills.",
+    subDescription: [
+      "Clean, responsive UI mimicking a social media platform",
+      "Built entirely with HTML, CSS, and JavaScript — no libraries",
+      "Includes features like navigation bar, post feed, and user cards",
+      " Mobile-friendly layout to demonstrate design adaptability",
+    ],
+    href: "https://sohamsarkarofficial-portfolio.netlify.app/social-index",
+    logo: "",
+    image: "/assets/projects/SocialApp.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      // {
+      //   id: 4,
+      //   name: "TailwindCSS",
+      //   path: "/assets/logos/tailwindcss.svg",
+      // },
+    ],
+  },
+  {
+    id: 3,
+    title: "Weather App",
+    description:
+      "A modern, interactive web application built with Blazor WebAssembly and .NET CoreA real-time weather app built with HTML, CSS, and JavaScript that fetches live weather data using a public API.",
+    subDescription: [
+      "Get current weather by city name with temperature, humidity, and conditions",
+      "Integrated with OpenWeatherMap API for real-time updates",
+      "Clean, responsive UI for both desktop and mobile views",
+      "Dynamic weather icons and background changes based on conditions",
+    ],
+    href: "https://sohamsarkarofficial-portfolio.netlify.app/socialweather",
+    logo: "",
+    image: "/assets/projects/WeatherApp.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Truth Dare Game",
+    description:
+      "A fun and interactive Truth or Dare web game built with HTML, CSS, and JavaScript, designed for parties, groups, and casual fun.",
+    subDescription: [
+      "Randomized truth and dare questions with smooth transitions",
+      "Gender-based and custom mode selection for personalized gameplay",
+      "Save and load custom question sets using localStorage",
+      "Mobile-friendly design with responsive layout and animations",
+    ],
+    href: "https://sohamsarkarofficial-portfolio.netlify.app/truth_dare",
+    logo: "",
+    image: "/assets/projects/GameApp.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "Tailwind",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 4,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "EliteTube",
+    description:
+      "EliteTube is a MERN stack-based content platform that enables users to subscribe to creators and engage in exclusive one-on-one paid chats with real-time messaging.",
+    subDescription: [
+      "Real-time chat using Socket.IO, with typing indicators and message timestamps",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Secure authentication with JWT and scalable backend powered by MongoDB Atlas.",
+      "Integrated Razorpay payment gateway for seamless paid subscriptions and chats",
+      "Supports media uploads via Cloudinary, with a clean UI built using Tailwind CSS"
+    ],
+    href: "https://elitetubelandingpage.netlify.app/",
+    logo: "",
+    image: "/assets/projects/EliteTube.png",
+    tags: [
+      {
+        id: 1,
+        name: "Nodejs",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 2,
+        name: "Express",
+        path: "/assets/logos/express.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongoDB.svg",
+      },
+      {
+        id: 4,
+        name: "Socket.io",
+        path: "/assets/logos/socket.io.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "Github",
+    href: "https://github.com/SohamSarkar025",
+    icon: "/assets/socials/githubcolor.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/soham-sarkar-85a5a6247/",
+    icon: "/assets/socials/linkedincolor.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "MERN FULL STACK DEVELOPER",
+    job: "Ex Intern at ARDENT COMPUTECH LTD.",
+    date: "2025-Present",
+    contents: [
+      "Learned and implemented backend development concepts using Node.js and Express.js",
+      "Worked with REST APIs, handling GET and POST requests efficiently",
+      "Gained hands-on experience in authentication and authorization using JWT",
+      "Integrated Nodemailer for sending emails (e.g., verification, contact forms)",
+      "Explored core Node.js modules and middleware logic",
+      "Built reusable frontend components using React.js and mastered React Hooks & props",
+      "Worked with MongoDB Atlas for data modeling and cloud database integration",
+      "Strengthened debugging, problem-solving, and full-stack implementation skills"
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: " Developer’s Note",
+    body: "“As a passionate final-year engineering student, I love building real-world web applications using the MERN stack. I focus on writing clean, maintainable code and improving with every project.”",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Learning Mindset",
+    body: "I believe in learning by doing — each project I’ve built, from a weather app to EliteTube, has taught me something new about backend logic, frontend polish, and real-time systems.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "Project Confidence",
+    body: "Projects like Talkify and EliteTube reflect my dedication to full-stack development — from frontend UI to secure backend APIs and real-time features",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Internship Experience",
+    body: "My internship at Ardent Computech Pvt. Ltd. gave me practical experience in API development, JWT authentication, Nodemailer, and building scalable apps using the MERN stack.",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Backend Mastery",
+    body: "I’ve gained confidence in handling backend tasks like protected routes, token-based auth, and email workflows — all while learning to write efficient and secure server code",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Frontend Growth",
+    body: "Through projects like my Truth or Dare game and weather app, I’ve improved my skills in responsive UI design, animations, and intuitive user experiences using Tailwind and vanilla JS.",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Collaboration Ready",
+    body: "I’m open to collaborating globally and always ready to learn from teams, communities, and mentors who push me to become a better developer.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Career Vision",
+    body: "My goal is to become a well-rounded software developer who can build user-friendly, secure, and scalable products that make an impact.",
+    img: "https://robohash.org/eve",
+  },
+];
